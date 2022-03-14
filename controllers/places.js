6a803545c3e4901af094f38ee2e2b5fa1506992a
Chris O'Brien <you@example.comchristopherwobrien03@gmail.com>
2022-03-14 18:20:42 -0500

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,24 +1,36 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
-router.get('/new', (req, res) => {
+router.get('/', (req, res) => {
+    res.render('places/index', {places})
+})
+
+router.post('/', (req, res) => {
+    // console.log(req.body)
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
+  })
+  
+  router.get('/new', (req, res) => {
     res.render('places/new')
   })
-router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: 'html.jpg'
-      }, {
-          name: 'Coding Cat Cafe',
-          city: 'Phoenix',
-          state: 'AZ',
-          cuisines: 'Coffee, Bakery',
-          pic: 'code cafe.jpg'
-      }]
-      res.render('places/index', {places})
-    })
-    
-
-module.exports = router
+  
+  router.get('/show', (req, res) => {
+    res.send('<h1>Show Page GET</h1>')
+  })
+  
+  router.get('/edit', (req, res) => {
+    res.send('<h1>Edit Page GET</h1>')
+  })
+  
+  module.exports = router
