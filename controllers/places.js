f46ec318ee91fc38f476bcb7e0a85b4e787e6c2d
Chris O'Brien <you@example.comchristopherwobrien03@gmail.com>
2022-03-14 19:37:31 -0500

    Navbar with style

--- controllers/places.js
+++ controllers/places.js
@@ -12,7 +12,7 @@ router.get("/new", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-  console.log("request = >", req.body);
+  //console.log("request = >", req.body);
   if (!req.body.pic) {
     // Default image if one is not provided
     req.body.pic = "http://placekitten.com/400/400";
